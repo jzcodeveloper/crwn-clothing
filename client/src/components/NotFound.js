88719e829af0396dba025ae7d9bf2eb5661b3bb2
Javier <javier_bislip@hotmail.com>
2020-01-20 00:40:18 -0300

    Fixed path of not found image

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import NotFoundImage from "../assets/404.png";
+
 const Overlay = styled.div`
   width: 100%;
   height: calc(100vh - 134px);
@@ -20,8 +22,6 @@ const Text = styled.h1`
   color: #2f8e89;
   margin: 30px 10px;
 `;
-
-import NotFoundImage from "../assets/404.png";
 
 const NotFound = () => (
   <Overlay>
